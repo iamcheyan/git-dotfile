e85c959c43fb3189ec93033bfcba22b8e72a0319
Vayn <vayn@vayn.de>
2011-05-03 03:18:27 +0800

    add template of c

---
+++ vim/templates/tpl.c
@@ -0,0 +1,12 @@
+/* <T_FILENAME> -
+ *
+ * Copyright (c) 2011, <T_AUTHOR> <<T_AUTHOR_EMAIL>>
+ * All rights reserved.
+ *
+ * Released under the MIT, BSD, and GPL Licenses.
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+
+#include <stdio.h>
+<T_CURSOR>
