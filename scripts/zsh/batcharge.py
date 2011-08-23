c38f02b90f21c98f316836261f10be75893384db
Vayn a.k.a. VT <vayn@vayn.de>
2011-08-24 03:53:33 +0800

    vcs prompt, ssh-agent, battery RPS, etc...

---
+++ scripts/zsh/batcharge.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8:
+
+import sys
+import math
+import subprocess
+
+p = subprocess.Popen(["acpitool", "-b"], stdout=subprocess.PIPE)
+output = p.communicate()[0]
+output = output.decode('utf8')
+
+status = [o.strip() for o in output.split(':', 1)[1].split(',')]
+
+total_slots, slots = 10, []
+filled = int(math.ceil(float(status[1][:-1])) * 0.1) * '▸'
+empty = (total_slots - len(filled)) * '▹'
+out = filled + empty
+
+color_green = '%{[32m%}'
+color_yellow = '%{[1;33m%}'
+color_red = '%{[31m%}'
+color_reset = '%{[00m%}'
+color_out = (
+  color_green if len(filled) > 6
+  else color_yellow if len(filled) > 4
+  else color_red
+)
+
+out = color_out + out + color_reset
+sys.stdout.write(out)
